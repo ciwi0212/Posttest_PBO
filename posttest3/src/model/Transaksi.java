5cd75b6a545e33f72f3ef958bd9ad6ac999a0b9b
CelliaAN <148638766+ciwi0212@users.noreply.github.com>
2025-03-30 01:31:11 +0800

    Add files via upload

---
+++ posttest3/src/model/Transaksi.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Transaksi {
+    private int id;
+    private Bunga bunga;
+    private int jumlah;
+    private double totalHarga;
+    private String pelanggan;
+    
+    public Transaksi(int id, Bunga bunga, int jumlah, String pelanggan, double totalHarga){
+        this.id = id;
+        this.bunga = bunga;
+        this.jumlah = jumlah;
+        this.totalHarga = bunga.getHarga() * jumlah;
+        this.pelanggan = pelanggan;
+    }
+
+    public int getID(){
+        return id;
+    }
+    public void setid(int id){
+            this.id = id;
+    }
+    public Bunga getBunga(){
+        return bunga;
+    }
+    public void setBunga(Bunga bunga){
+        this.bunga = bunga;
+    }
+    public int getJumlah(){
+        return jumlah;
+    }
+        
+    public void setJumlah(int jumlah){
+        this.jumlah = jumlah;
+    }
+    public double getTotalHarga(){
+        return totalHarga;
+    }
+    public void setTotalHarga(double totalHarga){
+        this.totalHarga = totalHarga;
+    }
+
+    public String getPelanggan(){
+        return pelanggan;
+    }
+
+    public void setPelanggan(String pelanggan){
+        this.pelanggan = pelanggan;
+    }
+}
